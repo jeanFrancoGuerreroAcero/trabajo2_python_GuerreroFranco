0223b1e62cef35ede940be295da25b1aa89f4a76
jeanFrancoGuerreroAcero <guerreroacerojeanfranco@gmail.com>
2024-08-09 08:20:26 -0500

    :sparkles: desarrollo de trabajo,json y readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,46 @@
 # TRABAJO DE README CON EJERCICIO PYTHON
+Se realizo un ejercicio en donde se debia realizar un software para una farmacia
+
+## General
+Descripcion: El software permite navegar entre si mediante menus que muestran opciones especificas, el programa cuenta opciones de:
+
+ventas:nos da diferentes opciones para registrar la venta
+
+compra:el menu y diseño nos pide unos datos especificos para basarlo en una fase de datos 
+
+uso: mediante las opciones que se da en los menus
+
+## Funcionamiento
+
+- Menus: cuenta con menus interactivos en donde estos le especifican al usuario las opciones disponibles dentro del programa
+
+## Lenguajes usados
+
+- Python
+
+## Estructura del proyecto
+
+## Caracteristicas
+cuenta con 3 json:
+ - json de medicamentos- cuenta con la informacion de los medicamentos y el stock
+
+- json de pacientes-cuenta con la informacion de el cliente que compro el producto y del empleado que realizo la venta
+
+- json de ventas-cuenta con la informacion del propvedor que surtio a la panaderia y de los productos que se compraron
+
+- el archivo de main.py contiene el funcionamiento del software
+
+- el json de empleado- cuenta con la informacion personal de el trabajador y que cargo tiene
+
+- el json de proovedores cuenta con la informacion de este
+
+- el json de compra cuenta con la informacion de las compras reañizadas 
+
+## Diseño
+
+- El diseño del software fue definido por una serie de requisitos propuestos en Notion
+Instrucciones de uso
+- ejecutar el programa desde la terminal de visual code o instalar la extension llamada code runer
+
+# Desarrolado por
+- jean franco guerrero acero, estudiante de Campusalands como trabajo de Readme con ejercicio Python
--- main.py
+++ main.py
@@ -1,1 +1,141 @@
-print("hola")
+import json
+
+####################
+##json de ventas
+
+def archivo():
+    miJSON=[]
+    with open('./ventas.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def archivoGuardadito(miData):
+    with open("./ventas.json","w") as outfile:
+        json.dump(miData,outfile)
+
+####################
+##json de pacientes 
+def archivoPacientes():
+    miJSON=[]
+    with open('./pacientes.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def GuardaditoPacientes(miData):
+    with open("./pacientes.json","w") as outfile:
+        json.dump(miData,outfile)
+
+####################
+##json de proovedores
+def archivoProovedores():
+    miJSON=[]
+    with open('./proovedores.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def GuardadoProovedores(miData):
+    with open("./proovedores.json","w") as outfile:
+        json.dump(miData,outfile)
+
+####################
+##json de medicamentos
+def archivoMedicamentos():
+    miJSON=[]
+    with open('./medicamentos.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def GuardaditoMedicamentos(miData):
+    with open("./medicamentos.json","w") as outfile:
+        json.dump(miData,outfile)
+
+####################
+##json de empleados 
+def archivoEmpleados():
+    miJSON=[]
+    with open('./empleados.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def GuardaditoEmpleados(miData):
+    with open("./empleados.json","w") as outfile:
+        json.dump(miData,outfile)
+
+##################
+##json de compras
+def archivoCompras():
+    miJSON=[]
+    with open('./compras.json','r') as openfile:
+        miJSON= json.load(openfile)
+    return miJSON
+
+def GuardaditoCompras(miData):
+    with open("./compras.json","w") as outfile:
+        json.dump(miData,outfile)
+
+
+print("----------------------")
+print("BIENVENIDO LA FARMACIA")
+print("----------------------")
+print("")
+print("que gestion desea realizar")
+print("1. ventas")
+print("2. compras")
+queopcion=input("Que opcion desea realizar :")
+
+booleano=True
+while booleano:
+    if queopcion=="1":
+        print("---------------------------------")
+        print("BIENVENIDO A LA SECCION DE VENTAS")
+        print("---------------------------------")
+        print("")
+        jsoonVentas=archivo()
+        for i in jsoonVentas:
+            queFecha=input("en que fecha se realizo la venta: ")
+            archivoGuardadito(queFecha)
+        jsoonVentas2=archivoPacientes()
+        for i in jsoonVentas2:
+            queNombre=input("que nombre tiene el paciente: ")
+            queDireccion=input("cual es la dirrecion de el cliente")
+            GuardaditoPacientes(queNombre,queDireccion)
+        jsoonVentas3=archivoEmpleados()
+        for i in jsoonVentas3:
+            queProovedor=input("cual es el nombre de el proovedor: ")
+            queCargo=input("que cargo tenia el proovedor: ")
+            GuardaditoEmpleados(queProovedor,queCargo)
+        jsoonVentas4=archivoMedicamentos()
+        for i in jsoonVentas4:
+            queProducto=input("cual es el nombre de el producto: ")
+            queCantidad=input("que cantidad tiene el producto: ")
+            quePrecio=input("que precio tiene la venta: ")
+            GuardaditoMedicamentos(queProducto,queCantidad,quePrecio)
+        booleano=False
+    if queopcion=="2":
+        print("----------------------------------")
+        print("BIENVENIDO A LA SECCION DE COMPRAS")
+        print("----------------------------------")
+        print("")
+        jsoonCompras=archivoCompras()
+        for i in jsoonCompras:
+            queFecha2=input("en que fecha se realizo la venta: ")
+            archivoGuardadito(queFecha2)
+        jsoonCompras2=archivoProovedores()
+        for i in jsoonCompras2:
+            queProovedor2=input("cual es el nombre de el proovedor: ")
+            archivoGuardadito(queProovedor2)
+        for i in jsoonVentas3:
+            queProovedor2=input("cual es el nombre de el proovedor: ")
+            queCargo2=input("que cargo tenia el proovedor: ")
+            GuardaditoEmpleados(queProovedor2,queCargo2)
+        jsoonCompras4=archivoCompras()
+        for i in jsoonCompras4:
+            queNombre3=input("que nombre tiene el producto: ")
+            queCantidad3=input("que cantidad de productos llevo: ")
+            quePrecio3=input("que precio tuvo la compra: ")
+            GuardaditoCompras(queNombre3,queCantidad3,quePrecio3)
+            booleano=False
+        
+
+        
+
